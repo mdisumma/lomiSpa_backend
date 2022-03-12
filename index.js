843c6e2d018da8170faad166e576fbd75807dcae
Marco Di Summa <mdisumma@yahoo.com>
2022-03-12 11:43:38 +0000

    build

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 import express from "express";
 import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
+
 import Services from "./services.js";
 import Users from "./users.js";
 import Booking from "./booking.js";
@@ -12,6 +15,12 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+console.log("/build", __dirname);
+
+app.use(express.static(path.join(__dirname, "/build")));
 //SERVICES
 Services(app);
 Users(app);
