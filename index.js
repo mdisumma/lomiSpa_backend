dea8d548e819c2c6d353e969ae4af5135f1df357
Marco Di Summa <mdisumma@yahoo.com>
2022-03-12 14:11:17 +0000

    path

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 import express from "express";
 import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
+
 import Services from "./services.js";
 import Users from "./users.js";
 import Booking from "./booking.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+console.log("/build", __dirname);
+console.log(path.join(__dirname, "/build", "index.html"));
+console.log(import.meta.url);
 
 // EXPRESS
 const app = express();
@@ -11,6 +21,7 @@ const PORT = 4000;
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "build")));
 
 //SERVICES
 Services(app);
@@ -19,5 +30,8 @@ Booking(app);
 
 //ROUTER
 app.get("/", (req, res) => res.send(`server running on port ${PORT}`));
+app.get("/", function (req, res) {
+	res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 
 app.listen(PORT, () => console.log(`server running on port ${PORT}`));
