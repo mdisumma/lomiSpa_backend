23e46c85142d71dc537f745c987c406bf2e6c84e
Marco Di Summa <mdisumma@yahoo.com>
2022-03-12 14:17:24 +0000

    min

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const PORT = 4000;
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "build")));
+// app.use(express.static(path.join(__dirname, "build")));
 
 //SERVICES
 Services(app);
