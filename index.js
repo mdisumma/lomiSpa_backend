059660d3ab2e3b1ae27e339b6330815ae453a786
Marco Di Summa <mdisumma@yahoo.com>
2022-03-17 10:08:39 +0000

    stripe

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import express from "express";
 import cors from "cors";
+import "dotenv/config";
+import Stripe from "stripe";
 
 import Services from "./services.js";
 import Users from "./users.js";
@@ -27,6 +29,33 @@ app.use(express.static(path.join(__dirname, "build")));
 Services(app);
 Users(app);
 Booking(app);
+
+//STRIPE PaymentGatway
+const stripe = new Stripe(process.env.STRIPE_SECRET_TEST);
+app.post("/payment", cors(), async (req, res) => {
+	let { amount, id } = req.body;
+	console.log(req.body);
+	try {
+		const payment = await stripe.paymentIntents.create({
+			amount,
+			currency: "usd",
+			description: "Lomi service",
+			payment_method: id,
+			confirm: true,
+		});
+		console.log("Payment", payment);
+		res.json({
+			message: "Payment successful",
+			success: true,
+		});
+	} catch (error) {
+		console.log("Error", error);
+		res.json({
+			message: "Payment failed",
+			success: false,
+		});
+	}
+});
 
 //ROUTER
 app.get("/", (req, res) => res.send(`server running on port ${PORT}`));
